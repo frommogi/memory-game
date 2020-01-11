13f2802126fae2fdd8ed8d4cc8ea57b92698d003
Mogi Batzorig <frommogi@gmail.com>
2020-01-11 14:44:38 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -5,7 +5,29 @@
 // console.log("User flipped" + cardOne);
 // console.log("User flipped" + cardThree);
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+},
+
+];
 const cardsInPlay = [];
 
 
@@ -27,10 +49,12 @@ function flipCard(cardId) {
 	// cardsInPlay.push(cardTwo);
 	// console.log("User flipped " + cardTwo);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
-	console.log("User flipped " + cards[cardId]);
-
+	console.log("User flipped " + cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
+		
 	if(cardsInPlay.length === 2) {
 		checkForMatch();
 	}
